53b9b01e238480bf00b535f39bda6c0ebe987844
Oscar Escobar <qomc16@gmail.com>
2022-05-09 22:14:12 -0600

    Primera prueba

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+//import { WebSocketService } from '../../services/web-socket.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css'],
+})
+export class ChatComponent implements OnInit {
+  userChat = {
+    user: '',
+    text: '',
+  };
+
+ // myMessages;
+  //eventName = 'send-message';
+
+  constructor(
+   //private activated: ActivatedRoute,
+   // private webService: WebSocketService
+  ) {}
+
+  ngOnInit(): void {
+    
+  }
+}
