96c0822d2ee493b72f2c3afa310fe2379a76597a
Oscar Escobar <qomc16@gmail.com>
2022-05-26 00:53:48 -0600

    Boton enviar

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { WebSocketService } from '../../services/web-socket.service';
-import {variable} from "@angular/compiler/src/output/output_ast";
+import { variable } from '@angular/compiler/src/output/output_ast';
+import { Socket } from 'socket.io-client';
 
 @Component({
   selector: 'app-chat',
@@ -13,39 +14,35 @@ export class ChatComponent implements OnInit {
   userChat = {
     user: '',
     text: '',
-
+    mensaje: '',
   };
-  colors = document.querySelectorAll('color-box label');
 
   myMessages: any;
-  messagecont = 0 ;
-  usercont=0;
-   eventName = 'send-message';
+  messagecont = 0;
+  usercont = 0;
+  eventName = 'send-message';
 
   constructor(
     private activated: ActivatedRoute,
     private webService: WebSocketService
-  ) {
-      }
+  ) {}
 
   ngOnInit(): void {
-
     const id = this.activated.snapshot.params['id'];
+    const contador = this.messagecont.toString();
     this.userChat.user = id;
+    this.userChat.mensaje = contador;
+    this.webService.emit('conectado',id);
     this.webService.listen('text-event').subscribe((data) => {
-    this.myMessages = data;
-
-
+      this.myMessages = data;
     });
-
-   }
+  }
 
   myMessage(): void {
     this.webService.emit(this.eventName, this.userChat);
     this.userChat.text = '';
     this.messagecont++;
-
+    const mensaje = this.messagecont.toString();
+    this.userChat.mensaje = mensaje;
   }
-
-
 }
