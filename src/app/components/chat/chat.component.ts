82331cdeeb59e4d77f4344f01d621a6cb46f532e
Oscar Escobar <qomc16@gmail.com>
2022-06-13 23:37:47 -0600

    Función esta escribiendo

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { HostListener } from '@angular/core';
 
@@ -6,6 +6,8 @@ import { WebSocketService } from '../../services/web-socket.service';
 import { variable } from '@angular/compiler/src/output/output_ast';
 import { Socket } from 'socket.io-client';
 import { Observable, retry } from 'rxjs';
+import { getLocaleDateFormat } from '@angular/common';
+import { outputAst } from '@angular/compiler';
 
 @Component({
   selector: 'app-chat',
@@ -13,24 +15,37 @@ import { Observable, retry } from 'rxjs';
   styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent implements OnInit {
+  div = document.createElement('type');
+  getData(val: string): void {
+    const id = this.activated.snapshot.params['id'];
+    this.contador++;
+    if(this.contador==1){
+      this.TypingData.id = id;
+      this.webService.emit('typing', this.TypingData);
+    }
+  }
   userChat = {
     user: '',
     text: '',
     mensaje: '',
-    tiempo:'',
+    tiempo: '',
   };
   userData = {
     id: '',
   };
+  TypingData = {
+    id: '',
+  };
+ contador=0;
   myUsers: any;
-  myUsersleft:any;
+  myUsersleft: any;
   myMessages: any;
+  myTyping: any;
   messagecont = 0;
   usercont = 0;
   eventName = 'send-message';
   @HostListener('window:beforeunload')
-  onUnload() {
-  }
+  onUnload() {}
   constructor(
     private activated: ActivatedRoute,
     private webService: WebSocketService
@@ -50,11 +65,15 @@ export class ChatComponent implements OnInit {
     });
     this.webService.listen('text-event').subscribe((data) => {
       this.myMessages = data;
-      console.log(this.myMessages);
+      this.myTyping=[]
     });
     this.webService.listen('usuariosd').subscribe((data) => {
       this.myUsersleft = data;
       console.log(this.myUsersleft);
+    });
+    this.webService.listen('escribiendo').subscribe((data) => {
+      this.myTyping = data+" Esta escribiendo";
+
     });
   }
 
@@ -65,12 +84,13 @@ export class ChatComponent implements OnInit {
     this.userChat.tiempo = Date.now().toString();
     this.webService.emit(this.eventName, this.userChat);
     this.userChat.text = '';
+    this.myTyping=[]
+    this.contador=0
   }
   salir(): void {
     const id = this.activated.snapshot.params['id'];
-    console.log("Si jala")
     this.userData.id = id;
-    this.webService.emit("desconectado",this.userData);
-    location.href="";
+    this.webService.emit('desconectado', this.userData);
+    location.href = '';
   }
 }
